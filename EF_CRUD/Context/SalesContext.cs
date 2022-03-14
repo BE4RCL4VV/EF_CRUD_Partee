dd860ecd953e153bb10c86a374bb18b15e6a572e
Mitch Partee <mitch.partee@student.centralia.edu>
2022-03-14 12:48:39 -0700

    Add project files.

---
+++ EF_CRUD/Context/SalesContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using EF_CRUD.Models;
+
+namespace EF_CRUD.Context
+{
+    public partial class SalesContext : DbContext
+    {
+        public static IConfiguration Configuration;
+        public SalesContext()
+        {
+        }
+
+        public SalesContext(DbContextOptions<SalesContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            Configuration = configuration;
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("conn"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.City).HasMaxLength(40);
+
+                entity.Property(e => e.Country).HasMaxLength(40);
+
+                entity.Property(e => e.FirstName).HasMaxLength(40);
+
+                entity.Property(e => e.LastName).HasMaxLength(40);
+
+                entity.Property(e => e.Phone).HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EF_CRUD/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using EF_CRUD.Context;
+using EF_CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace EF_CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SalesContext _context;
+
+        public HomeController(SalesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customers.ToListAsync());
+        }
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
